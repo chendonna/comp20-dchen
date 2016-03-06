dfc86db5d9ceff6dc114f6cf5b15809541163161
chendonna <chendonna@gmail.com>
2016-03-06 12:45:10 -0500

    modified lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,18 @@
 // Your JavaScript goes here...
+function parse() {
+	request = new XMLHttpRequest();
+	request.open("GET", "data.json", true);
+	request.onreadystatechange = function() {
+		if (request.readyState == 4) {
+			result = " ";
+			requestData = JSON.parse(request.responseText);
+			console.log(requestData);
+			elem = document.getElementById("messages");
+			for (var i = 0; i < requestData.length; i++) {
+				result = result + "<p class = 'usn'>" + requestData[i]["username"] + ": <span class = 'content'>"+requestData[i]["content"]+"</span></p>";
+			}
+			elem.innerHTML = result;
+		}
+	};
+	request.send(null);
+}
